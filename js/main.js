cc255d621b25d09b53532e2fa23c64fc8c10bad2
Jolane Synott <jolane@jolane.net>
2016-04-12 14:18:13 +1000

    Adding zooming image to landing section, fixes BH-12

--- js/main.js
+++ js/main.js
@@ -1,49 +1,54 @@
 jQuery(document).ready(function($) {
-    $('.menu__tab').click(function(event) {
+	$('.menu__tab').click(function(event) {
 
-        $(this).parent().find('.menu__tab').removeClass('is-active');
-        $(this).addClass('is-active');
+		$(this).parent().find('.menu__tab').removeClass('is-active');
+		$(this).addClass('is-active');
 
-        var thisIndex = $(this).index();
+		var thisIndex = $(this).index();
 
-        $('.menu__sections').removeClass('slide-0');
-        $('.menu__sections').removeClass('slide-1');
-        $('.menu__sections').removeClass('slide-2');
+		$('.menu__sections').removeClass('slide-0');
+		$('.menu__sections').removeClass('slide-1');
+		$('.menu__sections').removeClass('slide-2');
 
-        $('.menu__sections').addClass('slide-' + thisIndex);
+		$('.menu__sections').addClass('slide-' + thisIndex);
 
-    });
+	});
+
+
+	setTimeout(function() {
+		$('.header__zooing-image').addClass('is-zooming');
+	}, 1000);
 });
 
 
 var map;
 function initMap() {
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -37.808973, lng: 144.968828},
-        zoom: 16,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: false,
-    });
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -37.808973, lng: 144.968828},
+		zoom: 16,
+		scrollwheel: false,
+		navigationControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		draggable: false,
+	});
 
-    var icon = {
-        url: "/icons/icon-map-marker.svg",
-        anchor: new google.maps.Point(26,69),
-        scaledSize: new google.maps.Size(52,69)
-    }
+	var icon = {
+		url: "/icons/icon-map-marker.svg",
+		anchor: new google.maps.Point(26,69),
+		scaledSize: new google.maps.Size(52,69)
+	}
 
-    var marker = new google.maps.Marker({
-        position: {lat: -37.808973, lng: 144.968828},
-        map: map,
-        draggable: false,
-        icon: icon,
-        zIndex : -20
-    });
+	var marker = new google.maps.Marker({
+		position: {lat: -37.808973, lng: 144.968828},
+		map: map,
+		draggable: false,
+		icon: icon,
+		zIndex : -20
+	});
 
-    // add map styles
-    var styles = [{"stylers":[{"saturation":-100},{"gamma":0.8},{"lightness":4},{"visibility":"on"}]},{"featureType":"landscape.natural","stylers":[{"visibility":"on"},{"color":"#5dff00"},{"gamma":4.97},{"lightness":-5},{"saturation":100}]}]
-    map.setOptions({styles: styles});
+	// add map styles
+	var styles = [{"stylers":[{"saturation":-100},{"gamma":0.8},{"lightness":4},{"visibility":"on"}]},{"featureType":"landscape.natural","stylers":[{"visibility":"on"},{"color":"#5dff00"},{"gamma":4.97},{"lightness":-5},{"saturation":100}]}]
+	map.setOptions({styles: styles});
 }
