c7fa4a0277b332b3df492f32599896c524b3c767
Jolane Synott <jolane@jolane.net>
2016-05-31 09:02:24 +1000

    Fixed line height on intro text

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($) {
 	var current = 0;
 	function myCallback() {
 		current++;
-		var index = current%3;
+		var index = current%words.length;
 		console.log(index);
 		if($('.intro__is-text').hasClass('flip')) {
 			$('.intro__is-front').html(words[index]);
